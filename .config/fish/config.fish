8afba8e80287ac2309f433d371d755658acd5d59
Takuya OHASHI <tky.ohashi@gmail.com>
2018-03-14 14:52:45 +0900

    :hocho: [fish] split setting file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,16 @@
-# PATH
-set PATH ~/go/bin $PATH
+# environment
+. ~/.config/fish/env.fish
 
 # alias
-alias em="emacsclient -t"
+alias em "emacsclient -t"
+
+# local setting
+if test -e ~/.config/fish/local.fish
+    . ~/.config/fish/local.fish
+end
+
+if not test -f ~/.config/fish/functions/fisher.fish
+    echo "==> Fisherman not found.  Installing."
+    curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+    fisher
+end
---
+++ .config/fish/env.fish
@@ -0,0 +1,6 @@
+set -x LANG en_US.utf8
+
+# PATH
+set fish_user_paths ~/go/bin $fish_user_paths
+
+set -x EDITOR "emacs -nw"
