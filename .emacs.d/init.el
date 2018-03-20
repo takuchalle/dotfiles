618658c033e891752b854ba50f994cf08f2b459d
Takuya OHASHI <tky.ohashi@gmail.com>
2018-03-20 14:29:10 +0900

    [emacs] set key bind fo helm-show-kill-ring

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -63,6 +63,7 @@
 (require 'helm-config)
 (helm-mode 1)
 (global-set-key (kbd "M-x")     'helm-M-x)
+(global-set-key (kbd "M-y")     'helm-show-kill-ring)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
