9cb964a375e2bc11e59f706745e9c8c33cae7c89
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-18 09:47:49 +0900

    add rbenv path

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,7 +1,12 @@
 set -x LANG en_US.utf8
 
+if type rbenv > /dev/null;
+	status --is-interactive; and source (rbenv init -|psub)
+end
+
 # PATH
 set fish_user_paths ~/bin $fish_user_paths
+set fish_user_paths /usr/local/bin $fish_user_paths
 
 set -x GOPATH $HOME
 
