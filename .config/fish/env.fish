2a2d5ef151c2999c4387a0fe9ea2c4af1590c004
Takuya OHASHI <tky.ohashi@gmail.com>
2018-11-18 19:09:05 +0900

    change path priority

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,13 +1,12 @@
 set -x LANG en_US.utf8
 
-if type rbenv > /dev/null;
-	status --is-interactive; and source (rbenv init -|psub)
-end
-
 # PATH
 set fish_user_paths ~/bin $fish_user_paths
-set fish_user_paths /usr/local/bin $fish_user_paths
 
 set -x GOPATH $HOME
 
 set -x EDITOR "emacsclient -nw"
+
+if type rbenv > /dev/null;
+	status --is-interactive; and source (rbenv init -|psub)
+end
