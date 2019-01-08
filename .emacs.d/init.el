72d10f4bf8fdbe842834efc66ea88c23e087f8bb
Takuya OHASHI <tky.ohashi@gmail.com>
2019-01-08 18:55:28 +0900

    [emacs] change company-mode color setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -98,6 +98,21 @@
 (define-key company-search-map (kbd "C-n") 'company-select-next)
 (define-key company-search-map (kbd "C-p") 'company-select-previous)
 
+(set-face-attribute 'company-tooltip nil
+					:foreground "black" :background "white")
+(set-face-attribute 'company-tooltip-common nil
+					:foreground "black" :background "grey")
+(set-face-attribute 'company-tooltip-common-selection nil
+					:foreground "white" :background "blue")
+(set-face-attribute 'company-tooltip-selection nil
+					:foreground "black" :background "blue")
+(set-face-attribute 'company-preview-common nil
+					:background "black" :foreground "grey" :underline t)
+(set-face-attribute 'company-scrollbar-fg nil
+					:background "orange")
+(set-face-attribute 'company-scrollbar-bg nil
+					:background "gray40")
+
 ;; irony
 (package-install 'irony)
 (package-install 'company-irony)
