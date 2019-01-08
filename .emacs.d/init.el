51a8ad4a6bb1c2193a85f1e06ba614640f8c1dff
Takuya OHASHI <tky.ohashi@gmail.com>
2019-01-08 19:03:55 +0900

    [emacs] use eglot for c/c++

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -229,6 +229,12 @@
 (package-install 'lsp-ui)
 (package-install 'company-lsp)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; eglot
+(package-install 'eglot)
+(add-to-list 'eglot-server-programs '((c++ mode c-mode) . "clangd"))
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
 
 ;; snippet
 (package-install 'yasnippet)
