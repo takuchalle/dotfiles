194075583498bb251eee78238a0ac82bff83e454
takuchalle <me@takuchalle.dev>
2022-11-15 13:52:59 +0900

    Add leetcode template

---
+++ sonictemplate/cpp/base-leetcode.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	{{_cursor_}}
+}
+
+int
+main(int argc, char* argv[]) {
+	auto s = new Solution();
+
+	delete s;
+	return 0;
+}
