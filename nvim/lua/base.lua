4f26bd10315378708ce9a039aa79ccf9f2be805a
takuchalle <me@takuchalle.dev>
2023-06-01 18:42:00 +0900

    Update nvim

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -10,6 +10,7 @@ vim.wo.relativenumber = true
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.hlsearch = true
+vim.opt.incsearch = true
 vim.opt.backup = false
 vim.opt.showcmd = true
 vim.opt.cmdheight = 1
@@ -18,9 +19,18 @@ vim.opt.scrolloff = 10
 vim.opt.shell = 'zsh'
 vim.opt.inccommand = 'split'
 vim.opt.ignorecase = true
+
+-- indent
 vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
+-- color
+vim.opt.termguicolors = true
 
+vim.opt.visualbell = true
+vim.opt.showmatch = true
+
+vim.opt.winblend = 20
+vim.opt.pumblend = 20
